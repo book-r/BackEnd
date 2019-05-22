30c9c5ae74db6f8923282818aa571bdc6342d91a
Henry Blevins <heblevi@gmail.com>
2019-05-22 12:51:05 -0400

    truncate to del

--- data/seeds/00_down.js
+++ data/seeds/00_down.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   return knex('reviews').truncate()
-    .then(() => knex('users').truncate())
-    .then(() => knex('books_subjects').truncate())
-    .then(() => knex('subjects').truncate())
-    .then(() => knex('books_authors').truncate())
-    .then(() => knex('authors').truncate())
-    .then(() => knex('books').truncate())
-    .then(() => knex('publishers').truncate()) ;
+    .then(() => knex('users').del())
+    .then(() => knex('books_subjects').del())
+    .then(() => knex('subjects').del())
+    .then(() => knex('books_authors').del())
+    .then(() => knex('authors').del())
+    .then(() => knex('books').del())
+    .then(() => knex('publishers').del()) ;
 };
