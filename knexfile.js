6085f35b177ffbbd9238f946203935960661ed5a
Henry Blevins <heblevi@gmail.com>
2019-05-22 12:26:29 -0400

    Correct connection

--- knexfile.js
+++ knexfile.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
